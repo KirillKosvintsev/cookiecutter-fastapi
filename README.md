cbc3613f81086e378043d1edb733651e17b5bcdd
Кирилл Косвинцев <a@a.a>
2024-08-06 14:28:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# cookiecutter-fastapi
+
+In order to create a template to FastAPI projects. :rocket:
+
+## Important
+To use this project you don't need fork it. Just run cookiecutter CLI and voilà!
+
+## Cookiecutter
+
+Cookiecutter is a CLI tool (Command Line Interface) to create an application boilerplate from a template. It uses a templating system — Jinja2 — to replace or customize folder and file names, as well as file content.
+
+### How can I install?
+
+```bash
+pip install cookiecutter
+```
+
+### How can I generate a FastAPI project?
+
+```bash
+cookiecutter gh:arthurhenrique/cookiecutter-fastapi
+```
+
+### Hands On
+
+![Example](./docs/example/cookiecutter-fastapi-cli.svg)
---
+++ {{cookiecutter.project_slug}}/app/core/config.py
@@ -0,0 +1,30 @@
+import sys
+import logging
+
+from loguru import logger
+from starlette.config import Config
+from starlette.datastructures import Secret
+
+from core.logging import InterceptHandler
+
+config = Config(".env")
+
+API_PREFIX = "/api"
+VERSION = "{{cookiecutter.version}}"
+DEBUG: bool = config("DEBUG", cast=bool, default=False)
+MAX_CONNECTIONS_COUNT: int = config("MAX_CONNECTIONS_COUNT", cast=int, default=10)
+MIN_CONNECTIONS_COUNT: int = config("MIN_CONNECTIONS_COUNT", cast=int, default=10)
+SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret, default="")
+
+PROJECT_NAME: str = config("PROJECT_NAME", default="{{cookiecutter.project_name}}")
+
+# logging configuration
+LOGGING_LEVEL = logging.DEBUG if DEBUG else logging.INFO
+logging.basicConfig(
+    handlers=[InterceptHandler(level=LOGGING_LEVEL)], level=LOGGING_LEVEL
+)
+logger.configure(handlers=[{"sink": sys.stderr, "level": LOGGING_LEVEL}])
+
+MODEL_PATH = config("MODEL_PATH", default="{{cookiecutter.machine_learn_model_path}}")
+MODEL_NAME = config("MODEL_NAME", default="{{cookiecutter.machine_learn_model_name}}")
+INPUT_EXAMPLE = config("INPUT_EXAMPLE", default="{{cookiecutter.input_example_path}}")
